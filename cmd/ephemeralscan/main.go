1f4db0193fe9091344d453e8e5e8d3194c6cba7a
ivank-hvac <311254917+ivank-hvac@users.noreply.github.com>
2026-08-04 21:17:27 -0500

    refactor: move CLI into internal app package

--- cmd/ephemeralscan/main.go
+++ cmd/ephemeralscan/main.go
@@ -1,50 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
-const (
-	Version = "0.1.0-dev"
-)
+import "github.com/EphemeralScan/ephemeralscan/internal/app"
 
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-		return
-	}
-
-	switch os.Args[1] {
-
-	case "version":
-		version()
-
-	case "doctor":
-		doctor()
-
-	default:
-		fmt.Printf("Unknown command: %s\n\n", os.Args[1])
-		usage()
-		os.Exit(1)
-	}
-}
-
-func version() {
-	fmt.Printf("EphemeralScan %s\n", Version)
-}
-
-func doctor() {
-	fmt.Println("Doctor checks are not implemented yet.")
-}
-
-func usage() {
-	fmt.Println("EphemeralScan")
-	fmt.Println()
-	fmt.Println("Usage:")
-	fmt.Println("  ephemeralscan <command>")
-	fmt.Println()
-	fmt.Println("Available commands:")
-	fmt.Println("  version")
-	fmt.Println("  doctor")
+	app.Run()
 }
